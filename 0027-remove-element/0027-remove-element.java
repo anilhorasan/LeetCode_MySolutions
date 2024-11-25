7bd56acfbb8a4514c90a1e7d01229d753e5600a1
Anil Horasan <anilhorasan@gmail.com>
2024-11-25 08:01:57 -0500

    Time: 0 ms (100%), Space: 40.4 MB (99.99%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -2,14 +2,9 @@ class Solution {
     public int removeElement(int[] nums, int val) {
         int left = 0;
         for(int right = 0; right < nums.length; right++){
-            if(nums[right] != val) swap(nums, left++, right);
+            if(nums[right] != val) 
+                nums[left++] = nums[right];
         }
         return left;
-    }
-
-    public static void swap(int[] nums, int left, int right){
-        int temp = nums[left];
-        nums[left] = nums[right];
-        nums[right] = temp;
     }
 }
