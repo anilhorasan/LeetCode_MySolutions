73015e021bdf89c81a990fe3950c65e195331038
Anil Horasan <anilhorasan@gmail.com>
2024-12-10 17:19:42 -0500

    Time: 1 ms (99.9%), Space: 42.4 MB (29.32%) - LeetHub

--- 0067-add-binary/0067-add-binary.java
+++ 0067-add-binary/0067-add-binary.java
@@ -1,6 +1,90 @@
 class Solution {
+    public String addBinary4(String a, String b) {
+        int lenA = a.length();
+        
+        boolean carry = false;
+        String result = "";
+
+        for(int i = lenA-1; i >= 0; i--){
+            if(a.charAt(i) == '1' && b.charAt(i) == '1'){
+                if(carry) result = "1" + result;
+                else result = "0" + result;
+                carry = true;
+            }
+            
+            
+
+            else {
+                if(carry) {
+                    result = "1" + result;
+                    carry = false;
+                }
+                else result = "0" + result;
+            }
+        }
+        if (carry) return "1" + result;
+        return result;
+    }
+
+    public String addBinary5(String a, String b){
+
+        // use mutable strings in reverse
+        StringBuilder sb = new StringBuilder(); 
+        int carry = 0;
+        int t = 0;
+
+        // if flag is true, longest string is a, else longest string is b
+        boolean flag = a.length() >= b.length() ? true : false;
+        int lenOfLong = flag ? a.length() : b.length();
+        int lenOfShort = flag ? b.length() : a.length();
+        for(int i = lenOfLong-1; i >= 0; i--){
+            
+            if (flag)   {
+                t = a.charAt(i)- '0' + + carry;
+                if (lenOfShort > 0)   t += b.charAt(lenOfShort - 1)- '0' ;
+            }
+            else        {
+                t = b.charAt(i)- '0' + + carry;
+                if (lenOfShort > 0)   t += a.charAt(lenOfShort - 1)- '0' ;
+            }
+            lenOfShort--;
+
+            if (t == 0 || t == 1) {
+                carry = 0;
+            } else {
+                t = t%2;
+                carry = 1;
+            }
+            sb.append(Integer.toString(t));
+        }
+
+        if(carry == 1) sb.append(Integer.toString(carry));
+
+        return sb.reverse().toString();
+
+
+    }
 
     public String addBinary(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1, j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 ) {
+            int sum = carry;
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            if (j >= 0) sum += b.charAt(j--) - '0';
+
+            sb.append(sum % 2);  // Append the binary digit (0 or 1)
+            carry = sum / 2;     // Update carry
+        }
+        if (carry == 1) sb.append(Integer.toString(carry));
+
+    return sb.reverse().toString(); // Reverse and return the result
+}
+
+
+    public String addBinary3(String a, String b) {
         
         // use mutable strings in reverse
         StringBuilder sb = new StringBuilder(); 
@@ -23,79 +107,21 @@ class Solution {
         return sb.reverse().toString();
     }
 
-
+    
+    
     public String addBinary2(String a, String b) {
-        int lena = a.length();
-        int lenb = b.length();
-
-        String s = "";
-        int carry = 0;
-        char a1 = ' ';
-        char b1 = ' ';
-        for(int i = Math.max(lena, lenb)-1; i >= 0; i--){
-
-            if(lena > 0 || lenb > 0){
-                if (lena > 0 && lenb > 0){
-                    a1 = a.charAt(lena-1);
-                    b1 = b.charAt(lenb-1);
-                    if(carry == 0){
-                        if(a1 == b1 && a1 =='0') s = '0' + s;
-                        else if(a1 == b1 && a1 =='1') {
-                            s = '0' + s;
-                            carry = 1;
-                        }
-                        else s = '1' + s;
-                    }
-                    else{
-                        if(a1 == b1 && a1 =='0') {
-                            s = '1' + s;
-                            carry = 0;
-                        }
-                        else if(a1 == b1 && a1 =='1') s = '1' + s;
-                        else s = '0' + s;
-                    }
-                    lena--;
-                    lenb--;
-                }
-                else if (lena > 0){
-                    a1 = a.charAt(lena-1);
-                    if(a1 == '1' && carry == 0){
-                        s = '1' + s;
-                    }
-                    else if(a1 == '1' && carry == 1){
-                        s = '0' + s;
-                        carry = 1;
-                    }
-                    else if(a1 == '0' && carry == 0){
-                        s = '0' + s;
-                    }
-                    else if(a1 == '0' && carry == 1){
-                        s = '1' + s;
-                        carry = 0;
-                    }
-                    lena--;
-                }
-                else {
-                    b1 = b.charAt(lenb-1);
-                    if(b1 == '1' && carry == 0){
-                        s = '1' + s;
-                    }
-                    else if(b1 == '1' && carry == 1){
-                        s = '0' + s;
-                        carry = 1;
-                    }
-                    else if(b1 == '0' && carry == 0){
-                        s = '0' + s;
-                    }
-                    else if(b1 == '0' && carry == 1){
-                        s = '1' + s;
-                        carry = 0;
-                    }
-                    lenb--;
-                }
+        int lenA = a.length();
+        int lenB = b.length();
+        int longestStringLength = lenA >= lenB ? lenA : lenB;
+        boolean carry = false;
+        String result = "";
+        for(int i = longestStringLength-1; i >= 0; i--){
+            if(a.charAt(i) == '1' && b.charAt(i-lenA+lenB) == '1'){
+                if(carry) result += "1";
+                else result += "0";
+                carry = true;
             }
         }
-        if (carry == 1) s = '1' + s;
-        return s;
+        return " ";
     }
 }
