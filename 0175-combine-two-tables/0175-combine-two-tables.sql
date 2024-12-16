39423ab8fbf399ff77672ce54dc3614bf5855c59
Anil Horasan <anilhorasan@gmail.com>
2024-12-16 09:55:23 -0500

    Time: 758 ms (5.06%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,9 +1,28 @@
 -- Write your PostgreSQL query statement below
+select firstName, lastName, city, state
+from Person left join Address
+on Person.personId  = Address.personId;
+
+/*
 
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
+
+select p.firstName, p.lastName, a.city, a.state 
+from Person  p, Address a 
+where p.personId  = a.personId ;
+
+SELECT Person.firstName, Person.lastName, Address.city, Address.state 
+FROM Person 
+NATURAL LEFT JOIN Address;
+
+select firstName, lastName, city, state
+from Person left join Address
+on Person.personId  = Address.personId;
+
+*/
 
 /*
 -- Subquery with UNION
