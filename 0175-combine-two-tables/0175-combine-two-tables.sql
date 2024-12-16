a0171aef22ca0b368eb81485f4af754d8d60c85b
Anil Horasan <anilhorasan@gmail.com>
2024-12-16 09:54:42 -0500

    Time: 550 ms (5.06%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,11 +1,12 @@
 -- Write your PostgreSQL query statement below
-/*
+
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
-*/
 
+/*
+-- Subquery with UNION
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p, Address a
 WHERE p.personId = a.personId
@@ -13,3 +14,14 @@ UNION
 SELECT p.firstName, p.lastName, NULL AS city, NULL AS state
 FROM Person p
 WHERE p.personId NOT IN (SELECT personId FROM Address);
+
+
+-- Full Outer Join (If Supported)
+SELECT COALESCE(p.firstName, '') AS firstName, 
+       COALESCE(p.lastName, '') AS lastName, 
+       COALESCE(a.city, '') AS city, 
+       COALESCE(a.state, '') AS state
+FROM Person p
+FULL OUTER JOIN Address a
+ON p.personId = a.personId;
+*/
