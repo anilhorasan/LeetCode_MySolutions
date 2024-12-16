c99a5feb81ca0c555888562d15224c2d0adb818f
Anil Horasan <anilhorasan@gmail.com>
2024-12-16 09:47:19 -0500

    Time: 492 ms (6.16%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,5 +1,15 @@
 -- Write your PostgreSQL query statement below
+/*
 SELECT p.firstName, p.lastName, a.city, a.state
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
+*/
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p, Address a
+WHERE p.personId = a.personId
+UNION
+SELECT p.firstName, p.lastName, NULL AS city, NULL AS state
+FROM Person p
+WHERE p.personId NOT IN (SELECT personId FROM Address);
