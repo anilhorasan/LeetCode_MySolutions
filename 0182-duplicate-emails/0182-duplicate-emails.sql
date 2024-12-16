90c74771cdbd604ad53db1178e3646d285465e8c
Anil Horasan <anilhorasan@gmail.com>
2024-12-16 13:25:03 -0500

    Time: 616 ms (9.81%), Space: 0B (100%) - LeetHub

--- 0182-duplicate-emails/0182-duplicate-emails.sql
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -1,5 +1,10 @@
 -- Write your PostgreSQL query statement below
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
 
+/*
 SELECT p1.email as Email
 FROM Person p1
 JOIN Person p2
@@ -7,12 +12,7 @@ ON p1.email = p2.email
 WHERE p1.id < p2.id
 GROUP BY p1.email;
 
-/*
 
-SELECT email
-FROM Person
-GROUP BY email
-HAVING COUNT(email) > 1;
 
 
 SELECT p1.email as Email
