fd929ede92b33048b40b98e0d507f6325bc58da0
Anil Horasan <anilhorasan@gmail.com>
2025-01-02 21:59:45 -0500

    Time: 6 ms (25.09%), Space: 41.8 MB (39.82%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -0,0 +1,30 @@
+class Solution {
+    // 
+    // Time complexity: O(n) since we go over each element in the array
+    // Space complexity: O(1) we create 2 int, 1 boolean variables and an ArrayList, so O(4) => O(1)
+    public List<String> summaryRanges(int[] nums) {
+        List<String> result = new ArrayList<>();
+        if (nums.length == 0) return result;
+        int start_index = 0;
+        int last_num = nums[0];
+        boolean flag = false;
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] == last_num + 1){
+                flag = true;
+                last_num = nums[i];
+            } 
+            else {
+                if(flag)    result.add(nums[start_index] + "->" + nums[i-1]);
+                else        result.add(nums[i-1] + "");
+                flag = false;
+                last_num = nums[i];
+                start_index = i;
+            }
+        }
+        if(flag)    result.add(nums[start_index] + "->" + nums[nums.length-1]);
+        else        result.add(nums[nums.length-1] + "");
+        return result;
+        
+    }
+}
